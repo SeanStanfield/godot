a23b8a2a10696edff477375cab50f574dbfba48a
x1212 <x1212-x@web.de>
2015-08-08 14:13:34 +0000

    make.sh now builds release_debug with tools

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-scons platform=x11 LINKFLAGS=-L/opt/vc/lib\ -lbcm_host CCFLAGS=-I/opt/vc/include/\ -I/opt/vc/include/interface/vcos/pthreads/\ -I/opt/vc/include/interface/vmcs_host/linux/\ -fpermissive\ -march=armv7-a\ -mfpu=neon-vfpv4 CFLAGS=-I/opt/vc/include/\-I/opt/vc/include/interface/vcos/pthreads/\ -I/opt/vc/include/interface/vmcs_host/linux/\ -fpermissive -j4
+scons platform=x11 target=release_debug tools=yes LINKFLAGS=-L/opt/vc/lib\ -lbcm_host CCFLAGS=-I/opt/vc/include/\ -I/opt/vc/include/interface/vcos/pthreads/\ -I/opt/vc/include/interface/vmcs_host/linux/\ -fpermissive\ -march=armv7-a\ -mfpu=neon-vfpv4 CFLAGS=-I/opt/vc/include/\-I/opt/vc/include/interface/vcos/pthreads/\ -I/opt/vc/include/interface/vmcs_host/linux/\ -fpermissive -j4
 
